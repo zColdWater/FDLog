2521e2baa51e4a8a496e174ebdffa127527b6c18
yongpeng.zhu <yongpeng.zhu@nio.com>
2019-02-27 10:30:39 +0800

    add: notes on construct data model

--- FDLog-Demo/format_model/fd_construct_data.c
+++ FDLog-Demo/format_model/fd_construct_data.c
@@ -15,11 +15,14 @@ static const char *ismain_key = "m";
 FD_Construct_Data *
 fd_construct_json_data(char *log, int flag, long long local_time, char *thread_name,
                            long long thread_id, int is_main) {
+    
     FD_Construct_Data *construct_data = NULL;
     cJSON *root = NULL;
     fd_json_map *map = NULL;
+    
     root = cJSON_CreateObject();
     map = fd_create_json_map();
+    
     if (NULL != root) {
         if (NULL != map) {
             fd_add_item_string(map, log_key, log);
@@ -29,15 +32,23 @@ fd_construct_json_data(char *log, int flag, long long local_time, char *thread_n
             fd_add_item_number(map, threadid_key, (double) thread_id);
             fd_add_item_bool(map, ismain_key, is_main);
             fd_inflate_json_by_map(root, map);
+            
+            // 生成Jsonstring
             char *back_data = cJSON_PrintUnformatted(root);
+            // 为FD_Construct_Data申请堆内存
             construct_data = (FD_Construct_Data *) malloc(sizeof(FD_Construct_Data));
-
-            if (NULL != construct_data) {
+            
+            if (NULL != construct_data) { // 申请成功
+                // 清空 construct_data
                 memset(construct_data, 0, sizeof(FD_Construct_Data));
+                
+                // 得到日志jsonstring的内容长度
                 size_t str_len = strlen(back_data);
+                // 长度+1 在后面拼加 '\n' 换行符号。
                 size_t length = str_len + 1;
                 unsigned char *temp_data = (unsigned char *) malloc(length);
                 if (NULL != temp_data) {
+                    
                     unsigned char *temp_point = temp_data;
                     memset(temp_point, 0, length);
                     memcpy(temp_point, back_data, str_len);
@@ -46,22 +57,28 @@ fd_construct_json_data(char *log, int flag, long long local_time, char *thread_n
                     memcpy(temp_point, return_data, 1); //添加\n字符
                     construct_data->data = (char *) temp_data; //赋值
                     construct_data->data_len = (int) length;
+                    
                 } else {
+                    
                     free(construct_data); //创建数据
                     construct_data = NULL;
                     fd_printf(
                             "construct_json_data_clogan > malloc memory fail for temp_data\n");
                 }
             }
+            
             free(back_data);
         }
         cJSON_Delete(root);
     }
+    
     if (NULL != map) {
         fd_delete_json_map(map);
     }
+    
     return construct_data;
 }
+
 
 void fd_construct_data_delete(FD_Construct_Data *item) {
     if (NULL != item) {
--- FDLog-Demo/format_model/fd_construct_data.h
+++ FDLog-Demo/format_model/fd_construct_data.h
@@ -1,7 +1,6 @@
 #ifndef fd_construct_data_h
 #define fd_construct_data_h
 
-
 /**
  日志信息模型
  */
@@ -10,10 +9,26 @@ typedef struct {
     int data_len; // 日志长度
 } FD_Construct_Data;
 
+/**
+ 生成日志format模型
+
+ @param log 日志内容
+ @param flag 类型
+ @param local_time 系统当前时间
+ @param thread_name 线程名字
+ @param thread_id 线程id
+ @param is_main 是否主线程
+ @return FD_Construct_Data Object
+ */
 FD_Construct_Data *
 fd_construct_json_data(char *log, int flag, long long local_time, char *thread_name,
                            long long thread_id, int is_main);
 
+/**
+ 释放 FD_Construct_Data Object
+
+ @param data 日志format模型
+ */
 void fd_construct_data_delete(FD_Construct_Data *data);
 
 #endif
